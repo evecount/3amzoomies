0af382b0bc22b7ca74bcc1d451b53755d6c76fac
Gwendalynn Lim Wan Ting <gwenlynn.lim@gmail.com>
2025-11-22 22:04:52 +0000

    yeah something is weird with the left padding being off on the site

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="border-t border-border/40">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 md:flex-row md:gap-2">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
+      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0 px-4">
+        <div className="flex flex-col items-center gap-4 text-center md:text-left">
+          <p className="text-sm leading-loose text-muted-foreground">
             &copy; {new Date().getFullYear()} 3AMΣ. All Rights Reserved.
           </p>
         </div>
@@ -15,8 +15,8 @@ export function Footer() {
             <Link href="#" className="text-sm text-muted-foreground hover:text-foreground">Terms of Service</Link>
         </div>
       </div>
-      <div className="container pb-8 text-center">
-          <p className="text-sm text-foreground/80">
+      <div className="container pb-8 text-center px-4">
+          <p className="text-base text-foreground/80">
             This website is a satirical work of art. However, if enough people join the waitlist, we'll actually make the merch. No, really. ACTUALLY we'll assign a 'print to order' service close to their location and might even just send them the high res image if they are nice and they can make thier own damn shirt.
           </p>
       </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -35,7 +35,7 @@ const disabledIconTooltip = "This website is a satirical work of art. However, i
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 max-w-screen-2xl items-center px-4 md:px-8">
+      <div className="container flex h-16 max-w-screen-2xl items-center px-4">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <span className="font-bold sm:inline-block font-headline text-lg uppercase">3AMΣ</span>
