51ec321056c2a459de588c7115759ccdf47576a2
Gwendalynn Lim Wan Ting <gwenlynn.lim@gmail.com>
2025-11-22 16:40:22 +0000

    COOL now moretop paddng for those two elements, 10px

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -32,11 +32,11 @@ export function HeroSection() {
           <div className="group w-full max-w-md mx-auto">
             <AlbumArtDisplay />
           </div>
-          <div className="relative w-64 h-48 flex items-center justify-center -mt-8">
-            <div className="group w-40 absolute left-0">
+          <div className="relative w-80 h-64 flex items-center justify-center -mt-[12px]">
+            <div className="group w-52 absolute left-0">
               <AlbumArtDisplay />
             </div>
-            <div className="w-40 h-40 absolute right-0 -mr-10">
+            <div className="w-52 h-52 absolute right-0 -mr-12">
               <VinylRecord />
             </div>
           </div>
